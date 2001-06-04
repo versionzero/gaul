87282fad8d7cdfa1cc73e4322ed264a41520bd52
stewartadcock <stewartadcock>
2001-06-04 20:50:46 +0000

    New public header

---
+++ src/gaul.h
@@ -0,0 +1,47 @@
+/**********************************************************************
+  gaul.h
+ **********************************************************************
+
+  gaul - Genetic Algorithm Utility Library.
+  Copyright ©2000-2001, Stewart Adcock <stewart@bellatrix.pcl.ox.ac.uk>
+
+  The latest version of this program should be available at:
+  http://www.stewart-adcock.co.uk/
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.  Alternatively, if your project
+  is incompatible with the GPL, I will probably agree to requests
+  for permission to use the terms of any other license.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY WHATSOEVER.
+
+  A full copy of the GNU General Public License should be in the file
+  "COPYING" provided with this distribution; if not, see:
+  http://www.gnu.org/
+
+ **********************************************************************
+
+  Synopsis:	Public header file for GAUL.
+
+  Updated:      03/06/01 SAA	First version - currently just a stub.
+
+ **********************************************************************/
+
+#ifndef GAUL_H_INCLUDED
+#define GAUL_H_INCLUDED
+
+/*
+ * Includes
+ */
+#include "SAA_header.h"
+
+/*
+ * Include remainder of this libraries headers.
+ */
+#include "ga_util.h"
+#include "ga_intrinsics.h"
+
+#endif	/* GAUL_H_INCLUDED */
