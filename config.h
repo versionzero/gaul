1ea7c26db0ee2336fd371525513aa6d63e45df04
stewartadcock <stewartadcock>
2002-03-16 23:47:54 +0000

    Added data required by nnevolve example to the CVS archive.

--- config.h
+++ config.h
@@ -36,11 +36,11 @@
 
 /* #undef VERSION */
 #define VERSION_STRING "0.1837-2"
-#define BUILD_DATE_STRING "15/03/02"
+#define BUILD_DATE_STRING "16/03/02"
 #define MAJOR_VERSION 0
 #define MINOR_VERSION 1837
 #define PATCH_VERSION 2
-#define UNAME_STRING "Linux chemcca0 2.4.9-13smp #1 SMP Tue Oct 30 19:57:16 EST 2001 i686 unknown"
+#define UNAME_STRING "Linux desktop.stewart-adcock.co.uk 2.4.9-13 #1 Tue Oct 30 19:32:27 EST 2001 i686 unknown"
 
 #define AVLTREE_KEY_TYPE void *
 #define PARALLEL 0
