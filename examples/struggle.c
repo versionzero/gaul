a5959d324edc32a2ad8a07815df8f4d9e5cae3e7
stewartadcock <stewartadcock>
2005-04-11 09:49:45 +0000

    *** empty log message ***

--- examples/struggle.c
+++ examples/struggle.c
@@ -89,7 +89,7 @@ static boolean struggle_score(population *pop, entity *entity)
 
 int main(int argc, char **argv)
   {
-  int		i;			/* Loop over runs. */
+  int		i, j;			/* Loop over runs. */
   population	*pop=NULL;		/* Population of solutions. */
   char		*beststring=NULL;	/* Human readable form of best solution. */
   size_t	beststrlen=0;		/* Length of beststring. */
@@ -126,15 +126,18 @@ int main(int argc, char **argv)
        0.0              	/* double  migration */
                               );
 
+  for (j=0; j<50; j++)
+    {
     ga_evolution(
        pop,			/* population      *pop */
-       500			/* const int       max_generations */
+       50			/* const int       max_generations */
               );
 
-    printf( "The final solution with seed = %d was:\n", i );
+    printf( "The %d solution with seed = %d was:\n", j, i );
     beststring = ga_chromosome_char_to_string(pop, ga_get_entity_from_rank(pop,0), beststring, &beststrlen);
     printf("%s\n", beststring);
     printf( "With score = %f\n", ga_entity_get_fitness(ga_get_entity_from_rank(pop,0)) );
+}
 
     ga_extinction(pop);
     }
