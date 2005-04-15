edddc3ba459ce2efff4a14ea67264c8f378fa8f2
stewartadcock <stewartadcock>
2005-04-15 18:44:07 +0000

    Added license details into comments.

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,6 +1,33 @@
 #!/bin/sh
 #
-# Run GAUL test codes.
+#######################################################################
+# gaul-devel/tests/run_tests.sh
+#######################################################################
+#
+# GAUL - Genetic Algorithm Utility Library
+# Copyright ©2005, Stewart Adcock <stewart@linux-domain.com>
+# All rights reserved.
+#
+# The latest version of this program should be available at:
+# http://gaul.sourceforge.net/
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.  Alternatively, if your project
+# is incompatible with the GPL, I will probably agree to requests
+# for permission to use the terms of any other license.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY WHATSOEVER.
+#
+# A full copy of the GNU General Public License should be in the file
+# "COPYING" provided with this distribution; if not, see:
+# http://www.gnu.org/
+#
+#######################################################################
+#
+# Synopsis:	Run GAUL test codes.
 #
 
 echo "Running GAUL tests:"
